bfeddea4e6bfb23a52190c741227c3da194ffc35
giorgimazanishvili <giorgimazanishvili@gmail.com>
2024-12-26 17:47:04 +0400

    first commit

---
+++ day 01/homework/hw.py
@@ -0,0 +1,6 @@
+name = "giorgi"
+surname = "mazanishvili"
+heigth = 155
+age = 13
+nickname = "gio"
+print("hello my name is " + name + "  my surname is " + surname + " my height is  " + str(heigth) + "  my age is " + str(age) + " my nickname is " + nickname)
