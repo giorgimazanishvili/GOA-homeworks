7fea076f0ef5a803aadb37f5e38bdf0f42e63b4f
giorgimazanishvili <giorgimazanishvili@gmail.com>
2025-01-14 20:16:31 +0800

    next commit

---
+++ day 02/classwork/cw00.py
@@ -0,0 +1,10 @@
+name = "giorgi"
+age = 13
+height = 1.73
+
+print(name + " aris " + str(age) + " wlis da misi simaglea " + str(height) + " metri ")
+
+
+# srt() - string - ბრჭყალებში მოქცეული რაიმე ტექსტი ( მაგალითად : " გიორგი " )
+# int() - integer - რიცხვი ( მაგალითად : 50 )
+# float() - წილადი რიცხვი ( მაგალითად : 5.7 )
---
+++ day 02/classwork/cw02.py
@@ -0,0 +1,15 @@
+# print ფუნქციის მეშვეობით გამოიტანე ტერმინალში:
+
+#  5 + 5 = 10 
+#  10 - 5 = 5 
+#  5 * 5 = 25 
+#  10 / 5 = 2
+
+num1 = 5 
+num2 = 10
+
+print(str(num1) +  " + " + str(num1) + " = " + str(num1+num1))
+print(str(num2) +  " - " + str(num1) + " = " + str(num2-num1))
+print(str(num1) +  " * " + str(num1) + " = " + str(num1*num1))
+print(str(num2) +  " / " + str(num1) + " = " + str(num2//num1))
+
