ad13193deca38e3ba0f090344fd9de7e35aca290
giorgimazanishvili <giorgimazanishvili@gmail.com>
2025-01-14 21:19:06 +0800

    first commit

---
+++ day 02/homework/hw00.py
@@ -0,0 +1,16 @@
+
+
+# 2) შექმენით 4 ცვლადი თითოეულ მათგანს მნიშვნელობად მიანიჭეთ განსხავებული რიცხვები შემდეგ კი ამ რიცხვებით რომლებსაც ინახავთ ცვლადებში ყველა შესაძლო კომბინაციით შეასრულეთ ოთხი მათემატიკური მოქმედება: მიმატება, გამოკლება, გამრავლება, გაყოფა ისე რომ დაიბეჭდოს თვითონ მოქმედება და პასუხი მაგ. ავიღოთ ორი რიცხვი 3 და 4 ამ ორი რიცხვის შემთხვევაში შედექი იქნება 3 + 4 = 7 , 3 - 4 = -1 და ასე შემდეგ ყველა შესაძლო რიცხვების კომბინაციით და მოქმედებით, რეალური მაგალითი გავაკეთეთ გაკვეთილზე
+
+gela = 1
+gio = 2 
+saba = 4
+nika = 8
+
+# # + , - , * , / ...
+# # 1 + 2 + 4 + 8 = 15
+
+print( str(gela) + " + " + str(gio) + " + " + str(saba) + " + " + str(nika) + " = " + str( gela + gio + saba + nika ))
+
+print(" tayusgdawdsdfffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffftayusgdawdsdffffffffffffffffffffffffffffffffffffffffffffffffffff ")
+ 
