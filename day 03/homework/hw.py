d410a25afce4b16d0ae4768bbb1f63269e32fc12
giorgimazanishvili <giorgimazanishvili@gmail.com>
2025-01-16 03:17:09 +0800

    commit

--- day 03/homework/hw.py
+++ day 03/homework/hw.py
@@ -1,14 +1,17 @@
+
+gela = 1
+gio = 2 
+saba = 4
+nika = 8
 # 1 * 2 = 9
 # 1 * 4 = 5
 # 1 * 8 = 9
-
-
-
-
+print(str(gela) + " * " + str(gio)  + " = " + str(gela*gio))
+print(str(gela) + " * " + str(saba) + " = " + str(gela*saba))
+print(str(gela) + " * " + str(nika) + " = " + str(gela*nika))
 # 2 / 1 = 3
 # 4 / 1 = 5
 # 8 / 1 = 9.0
-
-
-
-
+print(str(gio) + " / " + str(gela) + " = " + str(gio/gela))
+print(str(saba) + " / " + str(gela) + " = " + str(saba/gela))
+print(str(nika) + " / " + str(gela) + " = " + str(nika/gela))
