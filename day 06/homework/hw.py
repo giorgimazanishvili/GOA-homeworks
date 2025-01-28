256b586cf5d3886721e6f62b5ef4951260ac830b
giorgimazanishvili <giorgimazanishvili@gmail.com>
2025-01-28 15:56:37 +0400

    next commit

---
+++ day 06/homework/hw.py
@@ -0,0 +1,9 @@
+
+num = int(input("Enter your number: "))
+
+if num < 0:
+    print("your number is negative ")
+elif num == 0:
+    print("your number = 0  ")
+else:
+    print("your number is positive ")
