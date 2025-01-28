b022a444d3a7ddec62fc8d1f2ab9318914d3293c
giorgimazanishvili <giorgimazanishvili@gmail.com>
2025-01-28 16:49:02 +0400

    first commit

--- day 05/homework/hw.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input("შემოიყვანეთ პირველი რიცხვი: "))
-num2 = int(input("შემოიყვანეთ მეორე რიცხვი: "))
-
-print(num1 + num2)
-print(num1 - num2)
-print(num1 * num2)
-print(num1 // num2)
-
