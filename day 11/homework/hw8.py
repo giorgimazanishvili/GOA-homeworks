f9ee799d50082feb1c542b206d709b0437aa2ce0
giorgimazanishvili <giorgimazanishvili@gmail.com>
2025-02-09 12:49:59 +0400

    next commit

---
+++ day 11/homework/hw8.py
@@ -0,0 +1,23 @@
+# 9)შემოატანინეთ მომხმარებელს რიცხვი და დაადგინეთ არის თუ არა დადებითი უარყოფითი ან ნულის ტოლი 
+num = int(input("Enter your number: "))
+
+if num < 0:
+    print("your number is negative ")
+elif num == 0:
+    print("your number = 0  ")
+else:
+    print("your number is positive ")
+
+
+num = int(input("Enter your number: "))
+
+if num < 0:
+    print("your number is negative ")
+elif num == 0:
+    print("your number = 0  ")
+else:
+    print("your number is positive ")
+
+
+
+
